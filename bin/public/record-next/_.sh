cd7ff94de45645a72b55c9bff83a4432121c7a02
da99 <i-hate-spam-1234567@mailinator.com>
2017-07-27 02:38:57 -0500

    I have no time to watch daily newsline.

--- bin/public/record-next/_.sh
+++ bin/public/record-next/_.sh
@@ -4,11 +4,11 @@
 record-next () {
   if [[ -z "$@" ]]; then
     local +x PATTERN="#TOKYO|Rising|Technology|Supernova|Cherry|Amazing Animals|Biz Lab|Insight|HOKUSAI|SOMEWHERE|COOL JAPAN|UKIYO-E|Doki|Document|Documentary|Buzz|Focus|Melo|Newsroom|Close-up|Inside"
-    case "$(date +"%I:%M")" in
-      "06:59") # Record NEWSLINE every 12 hours
-        PATTERN="NEWSLINE|$PATTERN"
-        ;;
-    esac
+    # case "$(date +"%I:%M")" in
+    #   "06:59") # Record NEWSLINE every 12 hours
+    #     PATTERN="NEWSLINE|$PATTERN"
+    #     ;;
+    # esac
 
   else
     local +x PATTERN="$@"
