45bba37e838c31bea216658df52a79e8099330bc
da99 <i-hate-spam-1234567@mailinator.com>
2017-08-06 07:01:46 -0500

    Added: save bandwidth on Comcast Cable.

--- bin/public/record-next/_.sh
+++ bin/public/record-next/_.sh
@@ -3,7 +3,11 @@
 
 record-next () {
   if [[ -z "$@" ]]; then
-    local +x PATTERN="#TOKYO|Rising|Technology|Supernova|Cherry|Amazing Animals|Biz Lab|Insight|HOKUSAI|SOMEWHERE|COOL JAPAN|UKIYO-E|Doki|Document|Documentary|Buzz|Focus|Melo|Close-up|Inside"
+    if lynx --dump "http://whatismyipaddress.com/" | grep "Comcast Cable" ; then
+      local +x PATTERN="Rising|Technology|Cherry|Biz Lab|Insight|HOKUSAI|COOL JAPAN|Document|Documentary|Buzz|Focus|Close-up|Inside"
+    else
+      local +x PATTERN="#TOKYO|Rising|Technology|Supernova|Cherry|Amazing Animals|Biz Lab|Insight|HOKUSAI|SOMEWHERE|COOL JAPAN|UKIYO-E|Doki|Document|Documentary|Buzz|Focus|Melo|Close-up|Inside"
+    fi
     # case "$(date +"%I:%M")" in
     #   "06:59") # Record NEWSLINE every 12 hours
     #     PATTERN="NEWSLINE|$PATTERN"
