505d2a0dd357ff429dd75c425117f700d6d77368
da99 <i-hate-spam-1234567@mailinator.com>
2017-08-19 05:18:42 -0500

    Dev. checkpoint.

--- bin/public/record-next/_.sh
+++ bin/public/record-next/_.sh
@@ -7,7 +7,7 @@ record-next () {
     if lynx --dump "http://whatismyipaddress.com/" | grep "Comcast Cable" ; then
       local +x PATTERN="$COMMON|Kuala|Somewhere|Rising|Technology|Cherry|Biz Lab|Insight|HOKUSAI|COOL JAPAN|Document|Documentary|Buzz|Focus|Close-up|Inside|MELO"
     else
-      local +x PATTERN="$COMMON|#TOKYO|Kuala|Somewhere|Rising|Technology|Supernova|Cherry|Amazing Animals|Biz Lab|Insight|HOKUSAI|SOMEWHERE|COOL JAPAN|UKIYO-E|Doki|Document|Documentary|Buzz|Focus|Melo|Close-up|Inside"
+      local +x PATTERN="$COMMON|#TOKYO|Kuala|Somewhere|Rising|Technology|Supernova|Cherry|Amazing Animals|Biz Lab|Insight|HOKUSAI|SOMEWHERE|COOL JAPAN|Doki|Document|Documentary|Buzz|Focus|Melo|Close-up|Inside"
     fi
     # case "$(date +"%I:%M")" in
     #   "06:59") # Record NEWSLINE every 12 hours
