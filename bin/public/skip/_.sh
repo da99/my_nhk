b11052e1c9c3aab5ec5cbad7a64ac9901c2f3aea
da99 <i-hate-spam-1234567@mailinator.com>
2018-01-26 08:42:20 -0600

    Fixed: skip

--- bin/public/skip/_.sh
+++ bin/public/skip/_.sh
@@ -1,8 +1,14 @@
 
 # === {{CMD}}  ID
 skip () {
-  cd "$THIS_DIR"
-  local +x ID="$1"; shift
-  mkdir -p tmp/skips
-  { my_nhk desc "$ID" || echo "$ID" ; } >> tmp/skips/$ID
+  local +x FILENAME="$(my_nhk record-info $1 | cut -d' ' -f3)"
+  local +x LIST="/play/nhk/list.txt"
+  if [[ ! -z "$FILENAME" ]] ; then
+    if ! grep "$FILENAME" "$LIST" >/dev/null ; then
+      echo nhk.$FILENAME.mp4 >> "$LIST"
+    fi
+    echo nhk.$FILENAME.mp4
+  else
+    exit 2
+  fi
 } # === end function
