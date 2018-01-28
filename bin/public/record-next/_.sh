e09832db05156ac9adb445898c275727a0783052
da99 <i-hate-spam-1234567@mailinator.com>
2018-01-28 13:23:14 -0600

    Dev. checkpoint.

--- bin/public/record-next/_.sh
+++ bin/public/record-next/_.sh
@@ -2,19 +2,8 @@
 # === {{CMD}}  Search String
 
 record-next () {
-  local +x COMMON="Technological|Toyama|SAPPORO|Songs of Tokyo|J-Trip|COOL|Theo Jansen|JAPANOLOGY mini|Close|Buzz|Sakamoto"
   if [[ -z "$@" ]]; then
-    # if lynx --dump "http://whatismyipaddress.com/" | grep "Comcast Cable" ; then
-      local +x PATTERN="$COMMON|Jakarta|Indo|Kuala|Technology|COOL JAPAN|Documentary|MELO"
-    # else
-    #   local +x PATTERN="$COMMON|Kuala|Technology|COOL JAPAN|Documentary|MELO"
-    # fi
-    # case "$(date +"%I:%M")" in
-    #   "06:59") # Record NEWSLINE every 12 hours
-    #     PATTERN="NEWSLINE|$PATTERN"
-    #     ;;
-    # esac
-
+    local +x PATTERN="TECHNOLOGICAL|TOYAMA|SAPPORO|SONGS OF TOKYO|J-TRIP|COOL|THEO JANSEN|JAPANOLOGY MINI|CLOSE|BUZZ|SAKAMOTO|JAKARTA|INDO|KUALA|TECHNOLOGY|COOL JAPAN|DOCUMENTARY|MELO"
   else
     local +x PATTERN="$@"
   fi
