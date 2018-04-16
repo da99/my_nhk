2063cf982ba2d88db73dae534907195dacba35b8
da99 <i-hate-spam-1234567@mailinator.com>
2018-04-16 15:34:17 -0500

    Development checkpoint.

--- bin/public/record-next/_.sh
+++ bin/public/record-next/_.sh
@@ -2,16 +2,6 @@
 # === {{CMD}}  Search String
 
 record-next () {
-  if [[ -z "$@" ]]; then
-    local +x PATTERN="Fukuoka|DOCUMENTARY|electronics|tempeh|tofu|Architectural|The Gifted|TECHNOLOGICAL"
-    PATTERN="$PATTERN|TOYAMA|SAPPORO|SONGS OF TOKYO|COOL|THEO JANSEN|JAPANOLOGY MINI|CLOSE|BUZZ|SAKAMOTO"
-    PATTERN="$PATTERN|JAKARTA|INDO|KUALA|TECHNOLOGY|COOL JAPAN|DOCUMENTARY|MELO"
-  else
-    local +x PATTERN="$@"
-  fi
-
-  my_nhk next-id "$PATTERN" | {
-    read -r ID
-    [[ -n "$ID" ]] && my_nhk record --silent $ID
-  }
+  local +x ID="$(my_media nhk next-favorite-id)"
+  [[ -n "$ID" ]] && my_nhk record --silent $ID
 } # === end function
